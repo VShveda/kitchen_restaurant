3375e9c5c1f4ddf417361ee35fac23c1e50f4500
Viktor Sveda <vinix.ru@gmail.com>
2024-07-18 17:43:11 +0300

    add DishType model class meta and Cook model

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,8 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class DishType(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self):
         return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "Cook"
+        verbose_name_plural = "Cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
