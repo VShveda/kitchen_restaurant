91a7749079ffef33bb60863b842c62f13c9823ad
Viktor Sveda <vinix.ru@gmail.com>
2024-07-18 18:24:04 +0300

    Fixed Cook class Meta

--- kitchen/models.py
+++ kitchen/models.py
@@ -16,8 +16,8 @@ class Cook(AbstractUser):
     years_of_experience = models.IntegerField(default=0)
 
     class Meta:
-        verbose_name = "Cook"
-        verbose_name_plural = "Cooks"
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
