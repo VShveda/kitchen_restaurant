452fc46a64383169ca42c9af819d8982fc4ddcf0
Viktor Sveda <vinix.ru@gmail.com>
2024-07-18 17:49:35 +0300

    add Dish model

--- kitchen/models.py
+++ kitchen/models.py
@@ -21,3 +21,17 @@ class Cook(AbstractUser):
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
