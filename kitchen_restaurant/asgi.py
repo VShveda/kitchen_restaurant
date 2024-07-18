ad52f2cbf33920b99f5c4c8660076bda67cf2d60
Viktor Sveda <vinix.ru@gmail.com>
2024-07-18 17:19:31 +0300

    used black to all project

--- kitchen_restaurant/asgi.py
+++ kitchen_restaurant/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for kitcher_restaurant project.
+ASGI config for kitchen_restaurant project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kitcher_restaurant.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kitchen_restaurant.settings")
 
 application = get_asgi_application()
