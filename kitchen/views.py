b12d66a26b9cda584dd7cb67af5f903e4fd88435
Viktor Sveda <vinix.ru@gmail.com>
2024-07-19 20:28:50 +0300

    add index.html function

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,5 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.views import generic
+
+from kitchen.models import (
+    Cook,
+    Dish,
+    DishType
+)
 
 
 def index(request):
-    return render(request, "kitchen/index.html")
+    dish_count = Dish.objects.count()
+    dish_types_count = DishType.objects.count()
+    cooks_count = Cook.objects.count()
+    context = {
+        "dish_count": dish_count,
+        "dish_types_count": dish_types_count,
+        "cooks_count": cooks_count
+    }
+    return render(
+        request,
+        "kitchen/index.html",
+        context=context
+    )
