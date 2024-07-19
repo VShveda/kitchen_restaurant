0fca326eb4f9ab6231defde59b5d0dfdd988156e
Viktor Sveda <vinix.ru@gmail.com>
2024-07-20 02:11:12 +0300

    add link to urls.py

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,13 +1,23 @@
-from django.contrib.auth import views
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from kitchen.views import index
+from kitchen.views import (
+    index,
+    DishTypeListView,
+    DishTypeCreateView,
+    DishTypeUpdateView,
+    DishTypeDeleteView
+)
 
 
 urlpatterns = [
     path("", index, name="index"),
-    path("login/", views.LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", views.LogoutView.as_view(next_page="login"), name="logout"),
+    path("login/", LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
+    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("dish-types/create/", DishTypeCreateView.as_view(), name="dish-type-create"),
+    path("dish-types/<int:pk>/update/", DishTypeUpdateView.as_view(), name="dish-type-update"),
+    path("dish-types/<int:pk>/delete/", DishTypeDeleteView.as_view(), name="dish-type-delete"),
 ]
 
 app_name = "kitchen"
