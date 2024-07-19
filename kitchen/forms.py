1278a1037d1ea86f0f0ea9531074ee39194cc75a
Viktor Sveda <vinix.ru@gmail.com>
2024-07-19 21:25:20 +0300

    add class CookCreationForm

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
 from kitchen.models import Cook, Dish
 
@@ -13,3 +14,11 @@ class DishForm(forms.ModelForm):
     class Meta:
         model = Dish
         fields = "__all__"
+
+
+class CookCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Cook
+        fields = UserCreationForm.Meta.fields + (
+            "years_of_experience",
+        )
