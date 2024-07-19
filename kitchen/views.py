3c99fbd371876240e4688a552507d4e53ab5b798
Viktor Sveda <vinix.ru@gmail.com>
2024-07-19 21:59:43 +0300

    add cook_assign_to_dish function

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
@@ -97,3 +98,12 @@ class CookExperienceUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Cook
     form_class = CookExperienceUpdateForm
     success_url = reverse_lazy("kitchen:cook-list")
+
+
+def cook_assign_to_dish(request, pk):
+    cook = Cook.objects.get(id=request.user.id)
+    if Dish.objects.get(id=request.user.id):
+        cook.dishes.remove(pk)
+    else:
+        cook.dishes.add(pk)
+    return HttpResponseRedirect(reverse_lazy("kitchen:dish-detail", args=[pk]))
