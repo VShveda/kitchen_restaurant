03ebc44ead0b6606f046cd3caa99118de291d8f5
Viktor Shveda <vinix.ru@gmail.com>
2024-07-20 00:00:25 +0300

    fix flack8

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -6,8 +6,8 @@ from kitchen.views import index
 
 urlpatterns = [
     path("", index, name="index"),
-    path("login/", views.LoginView.as_view(template_name='login.html'), name='login'),
-    path("logout/", views.LogoutView.as_view(next_page='login'), name='logout'),
+    path("login/", views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", views.LogoutView.as_view(next_page="login"), name="logout"),
 ]
 
 app_name = "kitchen"
