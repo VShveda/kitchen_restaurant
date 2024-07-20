ecb66cc7ff9da1fefb667ebccde98a26d131cd3d
Viktor Sveda <vinix.ru@gmail.com>
2024-07-20 18:06:57 +0300

    upgraded models.py

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 
 class DishType(models.Model):
@@ -18,11 +19,15 @@ class Cook(AbstractUser):
     years_of_experience = models.IntegerField(default=0)
 
     class Meta:
+        ordering = ["username"]
         verbose_name = "cook"
         verbose_name_plural = "cooks"
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("kitchen:cook-delete", kwargs={"pk": self.pk})
 
 
 class Dish(models.Model):
@@ -30,7 +35,7 @@ class Dish(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=7, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
-    cooks = models.ManyToManyField(Cook)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
 
     class Meta:
         ordering = ["name"]
