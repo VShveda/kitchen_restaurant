44ef31ef9f2443786d2887cb4dbb6dc0e47f204e
Viktor Sveda <vinix.ru@gmail.com>
2024-07-20 16:19:32 +0300

    add dish link to urls.py

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -6,7 +6,11 @@ from kitchen.views import (
     DishTypeListView,
     DishTypeCreateView,
     DishTypeUpdateView,
-    DishTypeDeleteView
+    DishTypeDeleteView,
+    DishListView,
+    DishCreateView,
+    DishUpdateView,
+    DishDeleteView
 )
 
 
@@ -18,6 +22,10 @@ urlpatterns = [
     path("dish-types/create/", DishTypeCreateView.as_view(), name="dish-type-create"),
     path("dish-types/<int:pk>/update/", DishTypeUpdateView.as_view(), name="dish-type-update"),
     path("dish-types/<int:pk>/delete/", DishTypeDeleteView.as_view(), name="dish-type-delete"),
+    path("dish/", DishListView.as_view(), name="dish-list"),
+    path("dish/create/", DishCreateView.as_view(), name="dish-create"),
+    path("dish/<int:pk>/update/", DishUpdateView.as_view(), name="dish-update"),
+    path("dish/<int:pk>/delete/", DishDeleteView.as_view(), name="dish-delete"),
 ]
 
 app_name = "kitchen"
