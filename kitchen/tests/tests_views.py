90d99cd5706eaef0b62a3f644b8741b080dfcd23
Viktor Sveda <vinix.ru@gmail.com>
2024-07-21 20:42:11 +0300

    add DishTypeCreateViewTests

--- kitchen/tests/tests_views.py
+++ kitchen/tests/tests_views.py
@@ -4,11 +4,13 @@ from django.contrib.auth import get_user_model
 from kitchen.models import Dish, DishType, Cook
 
 
-class IndexViewTests(TestCase):
+class LoginUserTestCase(TestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user(username="testuser", password="password")
         self.client.login(username="testuser", password="password")
 
+
+class IndexViewTests(LoginUserTestCase):
     def test_index_view(self):
         response = self.client.get(reverse("kitchen:index"))
         self.assertEqual(response.status_code, 200)
@@ -18,13 +20,17 @@ class IndexViewTests(TestCase):
         self.assertEqual(response.context["cooks_count"], Cook.objects.count())
 
 
-class DishTypeListViewTests(TestCase):
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(username="testuser", password="password")
-        self.client.login(username="testuser", password="password")
-
+class DishTypeListViewTests(LoginUserTestCase):
     def test_dish_type_list_view(self):
         response = self.client.get(reverse("kitchen:dish-type-list"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "kitchen/dish_types_list.html")
         self.assertEqual(response.context["dish_types_list"].count(), DishType.objects.count())
+
+
+class DishTypeCreateViewTests(LoginUserTestCase):
+    def test_dish_type_create_view(self):
+        response = self.client.post(reverse("kitchen:dish-type-create"), {"name": "New DishType"})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("kitchen:dish-type-list"))
+        self.assertTrue(DishType.objects.filter(name="New DishType").exists())
