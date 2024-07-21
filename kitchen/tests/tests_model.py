96e37449bb1c8c24ae297f9aed4fa5968f5080a5
Viktor Sveda <vinix.ru@gmail.com>
2024-07-21 21:19:50 +0300

    add CookModelTests

--- kitchen/tests/tests_model.py
+++ kitchen/tests/tests_model.py
@@ -12,3 +12,20 @@ class DishTypeModelTests(TestCase):
         self.assertEqual(dishtype._meta.verbose_name_plural, "dishtypes")
 
 
+class CookModelTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testcook",
+            password="password123",
+            first_name="Test",
+            last_name="Cook",
+            years_of_experience=5
+        )
+
+    def test_create_cook(self):
+        self.assertEqual(self.user.years_of_experience, 5)
+        self.assertEqual(str(self.user), f"testcook ({self.user.first_name} {self.user.last_name})")
+        self.assertEqual(self.user._meta.verbose_name, "cook")
+        self.assertEqual(self.user._meta.verbose_name_plural, "cooks")
+
+
