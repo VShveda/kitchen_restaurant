1cb40ef6ea72d4d7937053d3f12c796e41308797
Viktor Sveda <vinix.ru@gmail.com>
2024-07-21 20:34:03 +0300

    add DishTypeListViewTests

--- kitchen/tests/tests_views.py
+++ kitchen/tests/tests_views.py
@@ -16,3 +16,15 @@ class IndexViewTests(TestCase):
         self.assertEqual(response.context["dish_count"], Dish.objects.count())
         self.assertEqual(response.context["dish_types_count"], DishType.objects.count())
         self.assertEqual(response.context["cooks_count"], Cook.objects.count())
+
+
+class DishTypeListViewTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username="testuser", password="password")
+        self.client.login(username="testuser", password="password")
+
+    def test_dish_type_list_view(self):
+        response = self.client.get(reverse("kitchen:dish-type-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "kitchen/dish_types_list.html")
+        self.assertEqual(response.context["dish_types_list"].count(), DishType.objects.count())
