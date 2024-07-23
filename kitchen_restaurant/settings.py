b579de4c86ed785bebc72e48c3909646550d536d
Viktor Sveda <vinix.ru@gmail.com>
2024-07-23 11:29:31 +0300

    Update settings.py

--- kitchen_restaurant/settings.py
+++ kitchen_restaurant/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!qvaq5dl(@-r%_6%dw)ql$g+dj&gsp40c1h^-@65%a-=#4^@hg"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-!qvaq5dl(@-r%_6%dw)ql$g+dj&gsp40c1h^-@65%a-=#4^@hg")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") == "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
