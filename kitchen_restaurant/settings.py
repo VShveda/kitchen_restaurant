31510a0df426316cc2f72d072ff0cbbb9e614f96
Viktor Sveda <vinix.ru@gmail.com>
2024-07-23 15:29:25 +0300

    Staticfiles managing

--- kitchen_restaurant/settings.py
+++ kitchen_restaurant/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 import dj_database_url
+from dotenv import load_dotenv
 from pathlib import Path
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -46,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -87,9 +90,8 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=500, ssl_require=True)
 DATABASES["default"].update(db_from_env)
-DATABASES_URL = "postgresql://admin:OG4YKkiTUas9@ep-bold-forest-a2c49h1t.eu-central-1.aws.neon.tech/kitchen_restaurant?sslmode=require"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -128,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = "staticfiles"
 
 LOGIN_REDIRECT_URL = "/"
 
